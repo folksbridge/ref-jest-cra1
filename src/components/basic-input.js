d9687a5604e2e34581abf5833f07bd450c3373e8
Ken Wu <ken.wu@ibm.com>
2018-11-04 13:20:11 -0500

    first

---
+++ src/components/basic-input.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./basic-input.css";
+
+export default function BasicInput(props) {
+  return (
+    <input
+      type={props.type}
+      value={props.value}
+      className="basic-input"
+      placeholder={props.placeholder}
+      onChange={props.handleChanges}
+    />
+  );
+}
